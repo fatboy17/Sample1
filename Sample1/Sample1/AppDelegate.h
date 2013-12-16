aef2b8abf5beedb51390152487cee73c0d7e93d4
fatboy17 <Mac@Macintosh.local>
2013-12-16 10:11:42 +0700

    Add New Project

---
+++ Sample1/Sample1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample1
+//
+//  Created by Oat on 16/12/56.
+//  Copyright (c) พ.ศ. 2556 IT Easy 99. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
